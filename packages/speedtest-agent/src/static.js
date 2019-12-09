b0e580c7326faf477715509fd38cf994aecb3043
yelo <zhihuzeye@gmail.com>
2019-12-09 20:07:15 +0800

    refactor: mv speedtest-agent into its pkg dir

--- packages/speedtest-agent/src/static.js
+++ packages/speedtest-agent/src/static.js
@@ -3,6 +3,6 @@ const path = require('path')
 
 module.exports = async (req, res) => {
   await serve(req, res, {
-    public: path.resolve(__dirname, '../packages/speedtest-ui/dist'),
+    public: path.resolve(__dirname, '../../speedtest-ui/dist'),
   })
 }
